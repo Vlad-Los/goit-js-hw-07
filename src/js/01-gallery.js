cbbd6354b9bd994760ed5f46aebdae9151e17506
Vlad-Los <apperkot@gmail.com>
2022-12-26 19:08:04 +0200

    2612

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+// 1. создаем div-ы для preview изображений.
+// используем массив 
+const list = document.querySelector('.js-list')
+
+const markup = galleryItems.reduce((acc, url)=> acc + `<li><img src="${url}" alt="cat" loading="lazy" width="300"></li>`,'')
+list.insertAdjacentHTML("beforeend", markup)
+
+
+
+//for ()
+/*
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="preview"
+      data-source="original"
+      alt="description"
+    />
+  </a>
+</div>
+*/
+
+// 2. через querySelector задать событию onClick открытие original изображения
+/*
+document.querySelectorAll('gallery__image').onclick = () => {
+	basicLightbox.create(`
+		<img width="1400" height="900" src=target.dataset.source>
+	`).show()
+
+}
+
+*/
